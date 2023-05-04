49350a8d31a0d160f7bd51c4843595d6cf589440
InMysl <IvMysl2009@gmail.com>
2023-05-04 20:26:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const todoInput = document.querySelector(".todo__input");
+const addBtn = document.querySelector(".todo__btn");
+const todoList = document.querySelector(".todo__list");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+    if (todoInput.value !== ""){
+  const todo = document.createElement("li");
+  todo.innerHTML = `
+    <input type='checkbox'/>
+    <span>${todoInput.value}</span>
+    <button>Видалити</button>`;
+
+  todoList.appendChild(todo);
+  todoInput.value="";
+}
+});
+
+todoList.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'BUTTON'){
+        e.target.parentNode.remove();
+    }
+})
